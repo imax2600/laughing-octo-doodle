001f1f1336bf28d50608ab03e82ef4e56c383b6a
imax2600 <82081129+imax2600@users.noreply.github.com>
2024-03-18 09:30:17 +0000

    edit files

--- mod2/main.go
+++ mod2/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("123r4121io999hihi1hj212aas2112112kmllk21212212112112,asdad12212112cw 123cwe!\n")
+	fmt.Printf("123r4121ewrtrteio999hihi1hj212aas2112112kmllk21212212112112,asdad12212112cw 123cwe!\n")
 }
