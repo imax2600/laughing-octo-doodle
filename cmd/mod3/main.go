ac7d2f1016b77738c7bec1c11bc56c1ddb5511be
imax2600 <82081129+imax2600@users.noreply.github.com>
2024-03-19 03:22:23 +0000

    go

--- cmd/mod3/main.go
+++ cmd/mod3/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("1231fg3411211hjfgtyu112ashhherteterhuuu111un121233212121212112121, 123rf!\n")
+	fmt.Printf("1231fg3411211hjfgtyu112ashhherteterhuuu111unqq121233212121212112121, 123rf!\n")
 }
