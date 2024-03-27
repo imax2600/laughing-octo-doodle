4932970374b7d4c14f3b1933e59dc5bb5ce7abe9
imax2600 <82081129+imax2600@users.noreply.github.com>
2024-03-27 15:58:26 +0700

    test

--- cmd/mod1/main.go
+++ cmd/mod1/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HEHHEHHHEHEHEHEHEHEHEHHEE ww")
+	fmt.Fprintf(w, "HEHHEHHAASASHEHEHEHEHEHEHEHHEE ww")
 }
 
 func main() {
