346508c765633ffcf548ab82c00f98ca7f0b0fd0
imax2600 <82081129+imax2600@users.noreply.github.com>
2024-03-27 16:33:32 +0700

    test

--- cmd/mod1/main.go
+++ cmd/mod1/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HEasdhqHE!@# ww")
+	fmt.Fprintf(w, "TEHEHEEHEAS!@# ww")
 }
 
 func main() {
--- cmd/mod3/main.go
+++ cmd/mod3/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This11 is modu1l3211qweqw42e 3")
+	fmt.Fprintf(w, "This11 is modqweqweu1l3211qweqw42e 3")
 }
 
 func main() {
